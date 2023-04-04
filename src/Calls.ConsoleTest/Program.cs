e656295a8176a6adb695146088f69c0f7df82403
肖昕 <xiao.xin@outlook.com>
2023-04-04 12:53:53 +0800

    configure package information

--- src/Calls.ConsoleTest/Program.cs
+++ src/Calls.ConsoleTest/Program.cs
@@ -1,56 +1,21 @@
-﻿
-using MediatR;
+﻿using Calls;
 using Microsoft.Extensions.DependencyInjection;
 
-ServiceCollection serviceCollection = new();
-serviceCollection.AddMediatR(options =>
-{
-    options.RegisterServicesFromAssembly(typeof(BaseType).Assembly);
-});
 
-var services = serviceCollection.BuildServiceProvider();
+var call = new ServiceCollection()
+    .AddSingleton<ICall, MethodCall>()
+    .AddScoped<Handler>()
+    .BuildServiceProvider()
+    .GetRequiredService<ICall>();
 
-await services.GetService<IMediator>().Send(new RequestB());
-
-Test(typeof(long), typeof(int));
-
+Console.WriteLine(call.Invoke<int>(null, 1, 2));
 Console.ReadLine();
 
-void Test(Type a, Type b)
+[IncludeAssembly]
+public partial class MethodCall : Call { }
+
+public class Handler
 {
-
-    Console.WriteLine(a.IsAssignableFrom(b));
-    Console.WriteLine(b.IsAssignableFrom(a));
-}
-
-public class BaseType { }
-
-public class DeivedType : BaseType { }
-
-public class RequestHandler : IRequestHandler<RequestA>, IRequestHandler<RequestB>
-{
-    public Task Handle(RequestA request, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task Handle(RequestB request, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
-}
-
-public class RequestA : IRequest
-{
-
-}
-
-public class RequestB : RequestA
-{
-
-}
-
-public class RequestC : RequestB
-{
-
+    [Callable]
+    public int Add(int a, int b) => a + b;
 }
